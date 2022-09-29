92d354292a3371df6024d1dec05ee61fd0f98c5d
Fabian Silva <84555439+fabiansilva2@users.noreply.github.com>
2022-09-29 16:48:29 -0400

    Inserção dos Campos
    Header, Body, Footer, Button

---
+++ teste3/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Footer = () =>{
+    return(
+        <Text>Meu Footer</Text>
+    );
+}
+
+export default Footer;
---
+++ teste3/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+
+const Header = () =>{
+    return(
+        <Text>Meu Header</Text>
+    );
+}
+
+export default Header;
+
